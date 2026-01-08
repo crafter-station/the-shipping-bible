d6e5258e4be80638c85b9d766563bfa5d4360db6
Cris <cristian.correa.cs@gmail.com>
2026-01-08 13:51:56 -0500

    chore: add react and generate-audio script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://theshippingbible.com',
-	integrations: [sitemap(), mdx()],
+	integrations: [sitemap(), mdx(), react()],
 	markdown: {
 		shikiConfig: {
 			theme: 'github-dark',
